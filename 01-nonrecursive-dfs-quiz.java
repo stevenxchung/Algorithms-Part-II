fcb3b1a0346eba358c7ec126b26dea516e0d7f5b
Steven <stevenxchung@gmail.com>
2019-03-26 19:17:09 -0400

    Create 01-nonrecursive-dfs-quiz.java

---
+++ 01-nonrecursive-dfs-quiz.java
@@ -0,0 +1,19 @@
+// Nonrecursive Depth-frst Search
+// Pseudo-code
+
+public class NonrecursiveDFS {
+  // Adjacency lists
+  AdjacencyLists<Integer>[] adj;
+
+  // Construct graph
+  public NonrecursiveDFS(int V) {}
+
+  // Add edge
+  public void addEdge(int v, int w) {}
+
+  // Print unvisited vertices iteratively
+  public void printVertices(int s) {}
+
+  // Tests
+  public static void main(String[] args) {}
+}
