f682be754291605af0e8d54a8b69e23a756b967b
Steven <stevenxchung@gmail.com>
2019-03-26 19:17:15 -0400

    Create 03-euler-cycle-quiz.java

---
+++ 03-euler-cycle-quiz.java
@@ -0,0 +1,16 @@
+// Euler cycle
+// Pseudo-code
+
+public class EulerCycle {
+  // Check if vertex is even
+  public boolean isEven(int V) {}
+
+  // Returns true if all vertices are even
+  public boolean isEulerCycle(int[] v) {}
+
+  // Returns Euler Cycle only if graph has an Euler Cycle
+  public int[] EulerCycle(int[] v) {}
+
+  // Tests
+  public static void main(String[] args) {}
+}
