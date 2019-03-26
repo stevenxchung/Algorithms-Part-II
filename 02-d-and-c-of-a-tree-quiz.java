4dfe244d5b0694a1864975139731e932dcf2a929
Steven <stevenxchung@gmail.com>
2019-03-26 19:17:11 -0400

    Create 02-d-and-c-of-a-tree-quiz.java

---
+++ 02-d-and-c-of-a-tree-quiz.java
@@ -0,0 +1,13 @@
+// Diameter and center of a tree
+// Pseudo-code
+
+public class DiameterAndCenterOfATree {
+  // Returns true if path is longest from s to t
+  public boolean diameter(int s, int t) {}
+
+  // Returns vertex if V is farthest away from all v[]
+  public int center(int V, int[] v) {}
+
+  // Tests
+  public static void main(String[] args) {}
+}
