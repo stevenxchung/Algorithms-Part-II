782ca630b2312562a713b57c28e6cf2dbd6e31e5
Steven <stevenxchung@gmail.com>
2019-03-28 17:49:35 -0400

    Create 05-ham-path-in-a-dag-quiz.java

---
+++ Week 01/05-ham-path-in-a-dag-quiz.java
@@ -0,0 +1,13 @@
+// Hamilitonian path in a DAG
+// Pseudo-code
+
+public class HamilitonianPath {
+  // Returns true if evey vertex has a degree of two (inbound and outbound)
+  public boolean isHamilitonianPath(int[] v) {}
+
+  // Returns Hamilitonian path if every vertex is visited
+  public int[] HamilitonianPath(int[] v) {}
+
+  // Tests
+  public static void main(String[] args) {}
+}
