29947d4dcfd1338881b2590c0fb8a80b48a1abc9
Steven <stevenxchung@gmail.com>
2019-03-28 17:49:32 -0400

    Create 04-short-direct-cycle-quiz.java

---
+++ Week 01/04-short-direct-cycle-quiz.java
@@ -0,0 +1,16 @@
+// Shortest directed cycle
+// Pseudo-code
+
+public class ShortestDirectedCycle {
+  // Check if graph is acyclic
+  public boolean isAcyclic(int V) {}
+
+  // Returns true if edges are minimum
+  public boolean isShortestDirectedCycle(int[] v) {}
+
+  // Returns shortest directed cycle only if graph is not acyclic
+  public int[] ShortestDirectedCycle(int[] v) {}
+
+  // Tests
+  public static void main(String[] args) {}
+}
