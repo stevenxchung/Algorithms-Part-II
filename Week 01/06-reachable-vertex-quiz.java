12458a07c18c6ba70f7f1d3c60dfad9f9e1439b0
Steven <stevenxchung@gmail.com>
2019-03-28 17:49:38 -0400

    Create 06-reachable-vertex-quiz.java

---
+++ Week 01/06-reachable-vertex-quiz.java
@@ -0,0 +1,22 @@
+// Reachable vertex
+// Pseudo-code
+
+public class ReachableVertex {
+  // Returns true if no second mapping
+  public boolean isDAG(int[] v) {}
+
+  // Returns true if every vertex has a degree of two
+  public boolean isDigraph(int v) {}
+
+  // Returns true if a vertex is reachable by every other vertex
+  public boolean isReachableVertex(int[] v) {}
+
+  // Returns path if vertices are reachable and graph is DAG
+  public int[] reachableVertexDAG(int[] v) {}
+
+  // Returns path if vertices are reachable and graph is digraph
+  public int[] reachableVertexDigraph(int v) {}
+
+  // Tests
+  public static void main(String[] args) {}
+}
