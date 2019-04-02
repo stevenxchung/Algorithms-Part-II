b08e72d76c0e16a0b00a05c629490fb4b6c72168
Steven <stevenxchung@gmail.com>
2019-04-02 17:29:43 -0400

    Create 02-edge-in-mst-quiz.java

---
+++ Week 02/02-edge-in-mst-quiz.java
@@ -0,0 +1,10 @@
+// Is an edge in a MST?
+// Pseudo-code
+
+public class IsEdgeInMST {
+  // Helper function to check if edge appears
+  private boolean doesAppear(Edge e) {}
+
+  // Tests
+  public static void main(String[] args) {}
+}
