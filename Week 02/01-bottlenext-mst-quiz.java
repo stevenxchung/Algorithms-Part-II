bede38759df27c4e30213be360ba9ed33c4ae145
Steven <stevenxchung@gmail.com>
2019-04-02 17:29:39 -0400

    Create 01-bottlenext-mst-quiz.java

---
+++ Week 02/01-bottlenext-mst-quiz.java
@@ -0,0 +1,13 @@
+// Bottleneck minimum spanning tree
+// Pseudo-code
+
+public class BottleneckMST {
+  // Helper function to find smallest edge
+  private int[] findSmallestEdge(int[] a, int[] b) {}
+
+  // Build MST
+  public BottleneckMST(EdgeWeightedGraph G) {}
+
+  // Tests
+  public static void main(String[] args) {}
+}
