300a9967202ea727d5013c5e2c76dfdd0209a5c6
Steven <stevenxchung@gmail.com>
2019-04-03 19:50:50 -0400

    Create SeamCarver.java

---
+++ Week 02/seam/src/SeamCarver.java
@@ -0,0 +1,39 @@
+import edu.princeton.cs.algs4.Picture;
+
+public class SeamCarver {
+    // Create a seam carver object based on the given picture
+    public SeamCarver(Picture picture) {
+    }
+
+    // Current picture
+    public Picture picture() {
+    }
+
+    // Width of current picture
+    public int width() {
+    }
+
+    // Height of current picture
+    public int height() {
+    }
+
+    // Energy of pixel at column x and row y
+    public double energy(int x, int y) {
+    }
+
+    // Sequence of indices for horizontal seam
+    public int[] findHorizontalSeam() {
+    }
+
+    // Sequence of indices for vertical seam
+    public int[] findVerticalSeam() {
+    }
+
+    // Remove horizontal seam from current picture
+    public void removeHorizontalSeam(int[] seam) {
+    }
+
+    // Remove vertical seam from current picture
+    public void removeVerticalSeam(int[] seam) {
+    }
+}
