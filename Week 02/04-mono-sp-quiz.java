449458cb99b624b59c16693de71884cdf5a56e63
Steven <stevenxchung@gmail.com>
2019-04-03 18:13:59 -0400

    Create 04-mono-sp-quiz.java

---
+++ Week 02/04-mono-sp-quiz.java
@@ -0,0 +1,13 @@
+// Monotonic shortest path
+// Pseudo-code
+
+public class MonotonicSP {
+  // Does edge weight increase or decrease?
+  private boolean checkWeights(Edge e) {}
+
+  // Build SPT
+  public MonotonicSP(EdgeWeightedGraph G, int s) {}
+
+  // Tests
+  public static void main(String[] args) {}
+}
