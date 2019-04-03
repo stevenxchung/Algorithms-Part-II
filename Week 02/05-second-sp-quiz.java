a5137c02dc6c7395458a944c5bba6b17085d7b35
Steven <stevenxchung@gmail.com>
2019-04-03 18:14:04 -0400

    Create 05-second-sp-quiz.java

---
+++ Week 02/05-second-sp-quiz.java
@@ -0,0 +1,16 @@
+// Second shortest path
+// Pseudo-code
+
+public class SecondSP {
+  // Are weights strictly positive?
+  private boolean checkWeightsPositive(Edge e) {}
+
+  // Is new shortest path closest to P - the shortest path?
+  private boolean isSecondSP(int[] P, int s, int t) {}
+
+  // Build SPT
+  public SecondSP(EdgeWeightedDigraph G, int s, int t) {}
+
+  // Tests
+  public static void main(String[] args) {}
+}
