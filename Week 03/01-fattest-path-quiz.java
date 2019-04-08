afc4602131e4208fa3b80596442795f246db3c71
Steven <stevenxchung@gmail.com>
2019-04-08 19:51:20 -0400

    Create 01-fattest-path-quiz.java

---
+++ Week 03/01-fattest-path-quiz.java
@@ -0,0 +1,16 @@
+// Fattest path
+// Pseudo-code
+
+public class FattestPath {
+  // Helper function to check bottleneck capacity of a path
+  private boolean isBottleneckCapacity(FlowEdge e) {}
+
+  // Helper function to determine if bottleneck capacity is maximum
+  private boolean isFattestPath(FlowEdge e) {}
+
+  // Build Tree
+  public FattestPath(EdgeWeightedDigraph G) {}
+
+  // Tests
+  public static void main(String[] args) {}
+}
