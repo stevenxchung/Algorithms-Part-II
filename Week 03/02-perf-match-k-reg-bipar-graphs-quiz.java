15c2ce77bfbefd81e32039aa61527dd9aaa16b2f
Steven <stevenxchung@gmail.com>
2019-04-08 19:51:22 -0400

    Create 02-perf-match-k-reg-bipar-graphs-quiz.java

---
+++ Week 03/02-perf-match-k-reg-bipar-graphs-quiz.java
@@ -0,0 +1,16 @@
+// Perfect matchings in k-regular bipartite graphs
+// Pseudo-code
+
+public class PerfectMatchings {
+  // Helper function to check if relationships are mutual
+  private boolean isMutual(FlowEdge[] men, FlowEdge[] women) {}
+
+  // Helper function to check if acquaintance
+  private boolean isAcquaintance(FlowEdge man, FlowEdge woman) {}
+
+  // Build Tree
+  public PerfectMatchings(EdgeWeightedDigraph G) {}
+
+  // Tests
+  public static void main(String[] args) {}
+}
