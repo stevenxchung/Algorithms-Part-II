11722c65a71acf3e73673ec17d1f2282cac62cc6
Steven <stevenxchung@gmail.com>
2019-04-08 19:51:25 -0400

    Create 03-max-weight-closure-problem-quiz.java

---
+++ Week 03/03-max-weight-closure-problem-quiz.java
@@ -0,0 +1,16 @@
+// Maximum weight closure problem
+// Pseudo-code
+
+public class MaxWeightClosure {
+  // Helper function to check if digraph is closed
+  private boolean isClosed(EdgeWeightedDigraph G, FlowEdge e, int V) {}
+
+  // Helper function to check if set of vertices are max weight
+  private boolean isMaxWeight(int[] V) {}
+
+  // Build Tree
+  public MaxWeightClosure(EdgeWeightedDigraph G) {}
+
+  // Tests
+  public static void main(String[] args) {}
+}
