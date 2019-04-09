6a0b2682ca792c7a99c05f9156b2dc4bc3da33b4
Steven <stevenxchung@gmail.com>
2019-04-09 19:18:17 -0400

    Create 06-cyclic-rotations-quiz.java

---
+++ Week 03/06-cyclic-rotations-quiz.java
@@ -0,0 +1,19 @@
+// Cyclic rotations
+// Pseudo-code
+
+public class CyclicRotations {
+  // Helper function to determine if s and t are the same length
+  private boolean isSameLength(String s, String t) {}
+
+  // Helper function to determine if s consists of a suffix t followed by a prefix of t
+  private boolean checkSuffixAndPrefix(String s, String t) {}
+
+  // Helper function to determine if s and t are cyclic rotations of one another
+  private boolean isCyclicRotation(String s, String t) {}
+
+  // Determine if cyclic rotations exists for a pair of distinct strings in the array
+  public boolean CyclicRotations(String[] n) {}
+
+  // Tests
+  public static void main(String[] args) {}
+}
