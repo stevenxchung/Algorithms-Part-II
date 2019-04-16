044584841fc1878e8323099bb228ee5a59958821
Steven <stevenxchung@gmail.com>
2019-04-16 17:54:56 -0400

    Create 03-suffix-trees-quiz.java

---
+++ Week 04/03-suffix-trees-quiz.java
@@ -0,0 +1,19 @@
+// Suffix trees
+// Pseudo-code
+
+public class SuffixTree {
+  // Define a Node
+  private static class Node {}
+
+  // Helper function return queue of keys
+  private Iterable<String> keys() {}
+
+  // Helper function to build the queue of keys through in-order trie traversal from root to node
+  private void collect(Node x, String prefix, Queue<String> q) {}
+
+  // Build suffix tree
+  public SuffixTree(String s) {}
+
+  // Tests
+  public static void main(String[] args) {}
+}
