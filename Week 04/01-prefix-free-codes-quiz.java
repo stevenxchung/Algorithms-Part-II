e9e0f10d0c07a785c3ac2b66d84c03677d41d5e9
Steven <stevenxchung@gmail.com>
2019-04-16 17:54:50 -0400

    Create 01-prefix-free-codes-quiz.java

---
+++ Week 04/01-prefix-free-codes-quiz.java
@@ -0,0 +1,16 @@
+// Prefix free codes
+// Pseudo-code
+
+public class PrefixFreeCodes {
+  // Create a symbol table with string keys
+  private PrefixFreeCodes() {}
+
+  // Helper function to find all keys in symbol table starting with given prefix
+  private Iterable<String> keysWithPrefix(String prefix) {}
+
+  // Check if no string is a prefix of another
+  public boolean isPrefixFree(String[] s) {}
+
+  // Tests
+  public static void main(String[] args) {}
+}
