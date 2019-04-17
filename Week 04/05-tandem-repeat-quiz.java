0dd14684f1e8d465ab2417d2b5caa3c1214901ae
Steven <stevenxchung@gmail.com>
2019-04-17 17:52:18 -0400

    Create 05-tandem-repeat-quiz.java

---
+++ Week 04/05-tandem-repeat-quiz.java
@@ -0,0 +1,19 @@
+// Tandem Repeat
+// Pseudo-code
+
+public class TandemRepeat {
+  // Helper function to search for substring (KMP from lecture)
+  private KMP(String s) {}
+
+  // Helper function to check if base string b occurs at least once in string s
+  private boolean isTandemRepeat(String s, String b) {}
+
+  // Helper function to check if pattern p is the tandem substring of maximum length
+  private boolean isMaxLength(String p) {}
+
+  // Return the tandem substring of s which satisfies maximum length
+  public String maxTandemRepeat(String s, String b) {}
+
+  // Tests
+  public static void main(String[] args) {}
+}
