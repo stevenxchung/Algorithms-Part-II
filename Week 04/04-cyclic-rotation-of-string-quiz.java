30a45bd9ffdc87babffd6a5ead7273938cc1c4bb
Steven <stevenxchung@gmail.com>
2019-04-17 17:52:15 -0400

    Create 04-cyclic-rotation-of-string-quiz.java

---
+++ Week 04/04-cyclic-rotation-of-string-quiz.java
@@ -0,0 +1,16 @@
+// Cyclic rotation of a string
+// Pseudo-code
+
+public class CyclicRotation {
+  // Helper function to search for substring (KMP from lecture)
+  private KMP(String s) {}
+
+  // Helper function to check if both strings have the same length
+  private boolean isSameLength(String s, String t) {}
+
+  // True if cyclic rotation
+  public boolean isCyclicRotation(String s, String t) {}
+
+  // Tests
+  public static void main(String[] args) {}
+}
