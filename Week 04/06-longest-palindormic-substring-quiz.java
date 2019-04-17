07038ebd68fa6958bddf1f7746ee9d8c229de483
Steven <stevenxchung@gmail.com>
2019-04-17 17:52:20 -0400

    Create 06-longest-palindormic-substring-quiz.java

---
+++ Week 04/06-longest-palindormic-substring-quiz.java
@@ -0,0 +1,16 @@
+// Longest palindromic substring
+// Pseudo-code
+
+public class LongestPalindromicSubstring {
+  // Helper function to search for substring (KMP from lecture)
+  private KMP(String s) {}
+
+  // Helper function to check if string is a palindrome
+  private boolean isPalindrome(String s) {}
+
+  // Return the longest palindromic substring
+  public String isLongestPalindromicSubstring(String s) {}
+
+  // Tests
+  public static void main(String[] args) {}
+}
