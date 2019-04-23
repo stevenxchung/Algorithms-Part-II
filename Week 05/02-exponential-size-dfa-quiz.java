9c504f0fcfd6fdc5f6f009f51427593703c881fd
stevenxchung <stevenxchung@gmail.com>
2019-04-23 19:26:29 -0400

    Create 02-exponential-size-dfa-quiz.java

---
+++ Week 05/02-exponential-size-dfa-quiz.java
@@ -0,0 +1,20 @@
+// Exponential-size DFA
+// Pseudo-code
+
+public class ExponentialSizeDFA {
+  // Match transitions
+  private char[] re; 
+  // Epsilon transition digraph
+  private Digraph G; 
+  // Number of states
+  private int M; 
+
+  // Helper function to build diagraph
+  private Digraph buildDigraph() {}
+
+  // Build exponential-size DFA
+  public ExponentialSizeDFA (String regexp) {}
+
+  // Tests
+  public static void main(String[] args) {}
+}
