3665398b0c05bca73e1e1f988096bc12d112bc17
stevenxchung <stevenxchung@gmail.com>
2019-04-24 21:13:52 -0400

    Create CircularSuffixArray.java

---
+++ Week 05/burrows/src/CircularSuffixArray.java
@@ -0,0 +1,17 @@
+public class CircularSuffixArray {
+    // Circular suffix array of s
+    public CircularSuffixArray(String s) {
+    }
+
+    // Length of s
+    public int length() {
+    }
+
+    // Returns index of ith sorted suffix
+    public int index(int i) {
+    }
+
+    // Unit testing (required)
+    public static void main(String[] args) {
+    }
+}
