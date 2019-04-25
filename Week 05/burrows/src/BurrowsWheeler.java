408494d7b0e0ba2716dc46d49d364039f41307df
stevenxchung <stevenxchung@gmail.com>
2019-04-24 21:13:48 -0400

    Create BurrowsWheeler.java

---
+++ Week 05/burrows/src/BurrowsWheeler.java
@@ -0,0 +1,11 @@
+public class BurrowsWheeler {
+    // Apply Burrows-Wheeler transform, reading from standard input and writing to standard output
+    public static void transform() {}
+
+    // Apply Burrows-Wheeler inverse transform, reading from standard input and writing to standard output
+    public static void inverseTransform() {}
+
+    // If args[0] is '-', apply Burrows-Wheeler transform
+    // If args[0] is '+', apply Burrows-Wheeler inverse transform
+    public static void main(String[] args) {}
+}
