b16deae9d466ce4f80bdb247ff871396bd067c81
stevenxchung <stevenxchung@gmail.com>
2019-04-25 18:17:04 -0400

    Create 04-ternary-huffman-codes-quiz.java

---
+++ Week 05/04-ternary-huffman-codes-quiz.java
@@ -0,0 +1,16 @@
+// Ternary Huffman codes
+// Pseudo-code
+
+public class TernaryHuffman {
+  // Use Huffman Node data type
+  private static class Node implements Comparable<Node> {}
+
+  // Helper function to construct Huffman trie with ternary alphabet
+  private static Node buildTrie(int[] freq) {}
+
+  // Build Ternary Huffman
+  public TernaryHuffman() {}
+
+  // Tests
+  public static void main(String[] args) {}
+}
