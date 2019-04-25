c33c25923080f324aa2c25695d3e15999d048064
stevenxchung <stevenxchung@gmail.com>
2019-04-25 18:17:14 -0400

    Create 06-move-to-front-coding-quiz.java

---
+++ Week 05/06-move-to-front-coding-quiz.java
@@ -0,0 +1,19 @@
+// Move-to-front coding
+// Pseudo-code
+
+public class MoveToFrontCoding {
+  // Helper function to construct list of symbols
+  private String[] buildList(String s) {}
+
+  // Helper function to encode a symbol
+  private String encodeSymbol(String s) {}
+
+  // Helper function to move symbol to the front of the list and return the list of symbols
+  private String[] unshift(String[] symbols) {}
+
+  // Move-to-front encoding
+  public MoveToFrontCoding() {}
+
+  // Tests
+  public static void main(String[] args) {}
+}
