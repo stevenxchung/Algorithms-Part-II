1eb3a71ee15eab9987b0f0696c15652732fd2c0c
stevenxchung <stevenxchung@gmail.com>
2019-04-24 21:13:55 -0400

    Create MoveToFront.java

---
+++ Week 05/burrows/src/MoveToFront.java
@@ -0,0 +1,14 @@
+public class MoveToFront {
+    // Apply move-to-front encoding, reading from standard input and writing to standard output
+    public static void encode() {
+    }
+
+    // Apply move-to-front decoding, reading from standard input and writing to standard output
+    public static void decode() {
+    }
+
+    // If args[0] is '-', apply move-to-front encoding
+    // If args[0] is '+', apply move-to-front decoding
+    public static void main(String[] args) {
+    }
+}
